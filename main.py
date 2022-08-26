df5a78ef1da2ccb5175a59a7ed1696ca27b4c72e
acidf0x <ic_jdh@me.com>
2022-08-26 15:27:15 +0900

    first

---
+++ main.py
@@ -0,0 +1,21 @@
+# Python Stduy
+# 다른 언어와 다른점, 특장점 위주로 공부해보자
+
+# bool type -> 첫글자를 반드시 대문자로 써야한다
+print(True)
+print(False)
+
+# Null -> 파썬에서는 None으로 표현
+print(None)
+
+# 나누기, 몫연산자
+print(5/5) # 실수 리턴
+print(5//5) # 정수 리턴
+
+# 제곱 sqrt 안쓰네?
+print(5**2)
+
+#문자열 연산
+print("1" + "2") # 합쳐짐
+# print("1" + 1) 이건 실패함
+print("1" * 10) # 반복됨
