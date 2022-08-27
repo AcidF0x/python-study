5e42d29387c0f95eb19ff59899193e36fd0dfcef
Duhui Cho <ic_jdh@me.com>
2022-08-27 17:24:15 +0900

    update

--- basic.py
+++ basic.py
@@ -6,6 +6,8 @@
 """
 
 # bool type -> 첫글자를 반드시 대문자로 써야한다
+from ast import Lambda
+import enum
 from numbers import Number
 import this
 
@@ -59,6 +61,13 @@ print(animals[0])
 print(animals[0])
 del animals[0]
 print(animals[0])
+animals = [0, 11, 11, 11, 444, 555]
+animals.pop()
+print(animals,"----")
+animals.pop(0) # by index
+print(animals,"----")
+animals.remove(11) # by value 인덱스가 낮은순으로 지워진다
+print(animals,"----")
 
 ## append
 # animals[1] = 3 <- 에러남
@@ -129,9 +138,14 @@ for char in "this_is_lower_case":
 for i in range(1, 10): # 1 ~ 9 까지임
     print(i)
 
+# list loop with index
+for index, value in enumerate([1, 2, 3]):
+    print(f"index {index} / value {value}")
+
 # dictionary주의 사항 items()로 loop하지 않는다면 key만 가져오는 불상사가 나타난다
 for i in dictionary.items():
     print(i) ## i[0] => key, i[1]=> value
+
 
 # function
 def test(val):
@@ -140,7 +154,19 @@ def test(val):
 print(test(1))
 
 
+## 아래처럼 typehint 가능. 어디까지나 힌팅이라 오류가 발생하지 않는다
+## ??? 이건좀 신기한듯 IDE를 위한 기능으로 판단
 def test(name: str) -> int:
     return "123"
 
 print(test(123))
+
+
+# lambda
+
+print((lambda i: i+1)(100))
+
+#순서 조심해야할듯
+testing = lambda i: True if i > 100 else False
+
+print(testing(101))
--- class.py
+++ class.py
@@ -1,16 +1,20 @@
+from calendar import c
+
+
 class className:
     shareValue = 100
     """
-    해당 클래스의 인스턴스 들이 다 공유하는 클래스 변수
+    해당 클래스의 인스턴스 들이 다 공유하는 클래스의 속성
     이거 조금 웃기게 동작한다. 코틀린의 컴패니언 같은 느낌인데
     인스턴스로 접근하는것이 아니라 클래스명으로 접근한다
     """
-
     def __init__(self): # 생성자
         self.__number = 123 
         """
-        private 속성이라는 뜻으로 __를 붙인다 하지만 IDE를 위한 관습적? 키워드로
-        실제로 접근이 가능하다 파썬은 은닉화를 지원하지 않는듯 하다
+        private 속성이라는 뜻으로 __를 붙인다
+        __로 선언한경우 네임 맹글링을 통해 _클래스명__number로 변경이 된거라 사실 접근이 가능함 *
+        이름을 바꿔버리는 원리라 해당 이름으로 접근이 어렵게 만드는 원리
+
         """
     def increase(self):
         self.__number += 1
@@ -18,6 +22,22 @@ class className:
         self.__number -= 1
     def getNumber(self):
         return self.__number
+    def changeShareValue(self, value):
+        # 클래스 속성 접근시 이런식으로 클래스 명으로 접근하거나 클래스 메소드로 접근한다
+        className.shareValue = value
+    @classmethod
+    def changeShareValue2(cls, value):
+        """
+        클래스 메소드
+        @classmethod 데코레이터를 사용하여 self로 인스턴스를 전달하는 대신 클래스를 전달 받는다
+        static과 동일하게 인스턴스화 하지않아도 접근이 가능하지만 static과 다르게 클래스 속성에 접근이 가능하다
+        """
+        cls.shareValue = value
+    @staticmethod
+    def sayHi():
+        return "HI!!"
+
+    
 
 class childClass(className):
     pass #상속시 오버라이딩할 내용이 없다면 pass를 써줘야한다 뭔가 귀여운듯?
@@ -33,14 +53,25 @@ test = className()
 test.increase()
 test.decrease()
 print(test.getNumber())
+print(test._className__number) ## 맹글링 (__ 붙인것)은 접근이 요렇게 접근가능함
 
-className.shareValue = 2000 # 이렇게 쓰면 안되겠지? 초기화 시점은 언제인지?
+
+className.shareValue = 2000 # 클래스 속성 접근
 print(className.shareValue) 
 
+test.changeShareValue(111)
+print(className.shareValue)
+
+
+test.changeShareValue2(44444444)
+# 또는
+className.changeShareValue2(555555555)
+print(className.shareValue)
 
 test2 = child()
 test2.increase()
 print(test2.getNumber())
 
 
-
+print(className.sayHi())
+print(test2.sayHi())
